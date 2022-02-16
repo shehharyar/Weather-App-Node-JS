d6655be395c765d1e48d0ad55c268939986c67ae
shehharyar <sheharyarhassann1456@gmail.com>
2022-02-16 20:05:15 +0500

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-// const { response } = require("express");
-
-
-
 const weatherForm = document.querySelector('form');
 const search= document.querySelector('input');
 const msg1= document.getElementById('message-1')
